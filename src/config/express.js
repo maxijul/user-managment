f86ace1f7994ee92d7e370dfdfc595d9bfc36d96
maxijul <maxijul_14@hotmail.com>
2022-09-24 16:01:25 -0300

    add routes

--- src/config/express.js
+++ src/config/express.js
@@ -1,7 +1,17 @@
+import userRouter from '#Routes/user.routes.js';
 import express from 'express';
 
 const expressApp = express();
 
 // TODO añádir middlewares, routes...
+
+// Middlewares
+expressApp.use(express.json());
+
+
+
+// Routes
+expressApp.use('/user', userRouter);
+
 
 export default expressApp;
